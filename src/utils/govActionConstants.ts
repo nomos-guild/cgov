c0ac17e24d7c5d2e3f2482fc75631bc2f5e58f45
twwu123 <tszwaiwu.96@gmail.com>
2026-05-10 15:31:27 +0800

    add guardrail constants

---
+++ src/utils/govActionConstants.ts
@@ -0,0 +1,8 @@
+export const guardrailsScript = {
+  type: "PlutusScriptV3",
+  description: "",
+  cborHex: "46450101004981",
+};
+
+export const guardrailsScriptHash =
+  "914d97d63e2b7113465739faddd82362b1deaeedbcc4d01016c35c6e";
